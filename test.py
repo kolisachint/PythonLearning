dfc35870113afa8592e44499bd7423f1bdef0471
sachin koli <kolisachint@gmail.com>
2018-10-17 01:44:28 +0530

    2

--- test.py
+++ test.py
@@ -1,2 +1,32 @@
+# Python Program to find the factors of a number
 import sys
-print('sachin')
+
+sys.version
+print(sys.version)
+
+if condition:
+    pass
+else:
+    pass
+# define a function
+def print_factors(x):
+   # This function takes a number and prints the factors
+
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+           print(if condition:
+               pass
+           else:
+               pass)
+
+
+
+# change this value for a different result.
+num = 320
+
+# uncomment the following line to take input from the user
+#num = int(input("Enter a number: "))
+
+print_factors(num)
