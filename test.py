969633aa344e0b83bd4e36f11ddef6ee1289e225
kolisachint <kolisachint@gmail.com>
2018-10-30 10:12:10 -0400

    condenvy

--- test.py
+++ test.py
@@ -1,25 +1,12 @@
 # Python Program to find the factors of a number
 import sys
 
-sys.version
-print(sys.version)
-
-if condition:
-    pass
-else:
-    pass
 # define a function
 def print_factors(x):
    # This function takes a number and prints the factors
 
    print("The factors of",x,"are:")
-   for i in range(1, x + 1):
-       if x % i == 0:
-           print(i)
-           print(if condition:
-               pass
-           else:
-               pass)
+
 
 
 
@@ -27,6 +14,6 @@ def print_factors(x):
 num = 320
 
 # uncomment the following line to take input from the user
-#num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
 print_factors(num)
