61406e8e25b7afc4bb75bd211d88e42065970779
Koli <Sachin.Koli@searshc.com>
2018-11-27 20:30:53 +0530

    spell checker

--- Exercises/ex02.py
+++ Exercises/ex02.py
@@ -13,7 +13,7 @@ list_b = [1,2,3,4,5]
 print( list_a + list_b)
 
 list_a.extend(list_b)  					# concatenated list is not present in list_c
-print( list_a)									# In the above example, extends concatenates the list_b into list_a
+print( list_a)							# In the above example, extends concatenates the list_b into list_a
 
 
 numbers = [1, 2, 3, 4]
@@ -23,12 +23,12 @@ print( list(n**2 for n in numbers) )	# list comprehensions
 print( squares)
 
 print ( list( x**2 for x in numbers if x==2) , " this is my number 2")
-# Fuction with loop can be written in one line
+# Function with loop can be written in one line
 # let's check if lambda can be written this way
 
 print( list( map ( lambda x: x**2 , [1,2,3,4] )))		# map object
 print( list( n**2 for n in [1,2,3,4] if n==2 ) ) 		# Generator object, list comprehensions
-print( [ n**2 for n in [1,2,3,4] if n==2 ] ) 		# Generator object, list comprehensions
+print( [ n**2 for n in [1,2,3,4] if n==2 ] ) 		    # Generator object, list comprehensions
 # Don't need to call list function if its not absolutely necessary.Dont
 
 
---
+++ Exercises/ex03.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python
+# _*_ coding: utf-8 _*_
+# Decorators
+
+# Sample example illustrating decorators
+
+
