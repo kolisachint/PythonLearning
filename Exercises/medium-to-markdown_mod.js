c9daa4f17da7132999c766863860683251c6a440
Koli <Sachin.Koli@searshc.com>
2018-11-27 19:30:12 +0530

    Removing unnecessary junk

--- Exercises/medium-to-markdown_mod.js
+++
@@ -1,5 +0,0 @@
-const mediumToMarkdown = require('medium-to-markdown');
- 
-mediumToMarkdown.convertFromUrl("https://medium.com/@Alikayaspor/how-to-use-your-kindle-more-effective-to-cultivate-a-reading-habit-a61f0fe45f1d").then(function (markdown) {
-  console.log(markdown); //=> Markdown content of medium post
-});
